7c2df2730e6879f6e4f045d51188ab82904f54a0
khushi arzare <khushiarzare525@gmail.com>
2025-01-07 11:49:22 +0530

    7

--- src/Cont.jsx
+++ src/Cont.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 export default function Cont() {
   return (
     <div >
-    fgfghf
+    <div>
+    <p>Name : <br />
+       <input type="text"  className='p-1 border-stone-500 w-96'/></p>  
+    <p>Email :  <br />
+       <input type="text" className='p-1 border-stone-500  w-96' />
+    </p>
+    <p> What can I help you with ? <br />
+      <input type="text"   />
+    </p>
+    <p>
+      What would make you happy in this project ? <br />
+      <input type="text" className='p-1 border-stone-500  w-96' />
+    </p>
+    </div>
     </div>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Home() {
   return (
     <div >
-      <p>t  </p>
+      <div>
+     <div>
+      <p>Hello!! <br /> I'm khushi Arzare
+      </p>
+      <h1>I'm A WEB DESIGNER</h1>
+     </div>
+     <div></div>
+      </div>
+      
     </div>
   )
 }
