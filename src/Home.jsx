3cb7ccf68efcb7601a1e71f7acdcfa1e9e96f88f
khushi arzare <khushiarzare525@gmail.com>
2025-01-09 16:03:28 +0530

    adding

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import khushi from '../src/image/khushi.png';
 
 export default function Home() {
   return (
     <div >
-      <div>
+      <div className='flex'>
      <div>
       <p>Hello!! <br /> I'm khushi Arzare
       </p>
       <h1>I'm A WEB DESIGNER</h1>
      </div>
-     <div></div>
+     <div>
+      <img src={khushi} alt=""  className='rounded-full h-96  '/>
+     </div>
       </div>
       
     </div>
